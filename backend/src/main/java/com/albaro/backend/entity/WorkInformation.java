f43623fc904cdd622424d0d8c960367c82ffe62e
Yu Junghyun <a1017u@naver.com>
2025-02-10 17:14:43 +0900

    Feat: 공통 엔티티 생성

---
+++ backend/src/main/java/com/albaro/backend/entity/WorkInformation.java
@@ -0,0 +1,155 @@
+// WorkInformation.java
+package com.project.albaro.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "workInformation")
+@Builder
+public class WorkInformation {
+
+    // 스케줄 고유 ID
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "scheduleId", columnDefinition = "UNSIGNED INT")
+    private Integer scheduleId;
+
+    // WorkInformation - store 관계 -> 다 대 1 : 여러개의 근무 정보가 하나의 지점에 연결 가능
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "storeId", nullable = false, foreignKey = @ForeignKey(name = "FK_workInformation_store"))
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Store store;
+
+    // 근무정보 - 사용자 관계 -> 다 대 1
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "userId", nullable = false,  foreignKey = @ForeignKey(name = "FK_workInformation_user"))
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;// 근무자
+
+    @Column(name = "workDate", nullable = false)
+    private LocalDate workDate;    // 근무 날짜
+
+    @Column(name = "startTime", nullable = false)
+    private LocalTime startTime;    // 시작 시간
+
+    @Column(name = "endTime", nullable = false)
+    private LocalTime endTime;    // 마감 시간
+
+    @Column(name = "isVacant", nullable = false)
+    private Boolean isVacant;    // 공석 여부
+
+    @Column(name = "checkInTime")
+    private LocalTime checkInTime;    // 알바생 출근 시간
+
+    @Column(name = "checkOutTime")
+    private LocalTime checkOutTime;    // 알바생 퇴근 시간
+
+    @Column(name = "realTimeWorker")
+    private int realTimeWorker;    // 실제 근무자
+    // 실제 근무자 - 대타 파악 용도
+    // user: 원래 근무 배정자(외래키),  realTimeWorker: 실제 근무자
+    // user == realTimeWorker: 배정 받은 사람이 근무함, user != realTimeWorker: 대타 받음
+
+
+    public WorkInformation(){
+
+    }
+
+    public WorkInformation(Integer scheduleId, Store store, User user, LocalDate workDate, LocalTime startTime, LocalTime endTime, Boolean isVacant, LocalTime checkInTime, LocalTime checkOutTime, int realTimeWorker) {
+        this.scheduleId = scheduleId;
+        this.store = store;
+        this.user = user;
+        this.workDate = workDate;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.isVacant = isVacant;
+        this.checkInTime = checkInTime;
+        this.checkOutTime = checkOutTime;
+        this.realTimeWorker = realTimeWorker;
+    }
+
+    public Integer getScheduleId() {
+        return scheduleId;
+    }
+
+    public void setScheduleId(Integer scheduleId) {
+        this.scheduleId = scheduleId;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
+    public void setStore(Store store) {
+        this.store = store;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDate getWorkDate() {
+        return workDate;
+    }
+
+    public void setWorkDate(LocalDate workDate) {
+        this.workDate = workDate;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public Boolean getVacant() {
+        return isVacant;
+    }
+
+    public void setVacant(Boolean vacant) {
+        isVacant = vacant;
+    }
+
+    public LocalTime getCheckInTime() {
+        return checkInTime;
+    }
+
+    public void setCheckInTime(LocalTime checkInTime) {
+        this.checkInTime = checkInTime;
+    }
+
+    public LocalTime getCheckOutTime() {
+        return checkOutTime;
+    }
+
+    public void setCheckOutTime(LocalTime checkOutTime) {
+        this.checkOutTime = checkOutTime;
+    }
+
+    public int getRealTimeWorker() {
+        return realTimeWorker;
+    }
+
+    public void setRealTimeWorker(int realTimeWorker) {
+        this.realTimeWorker = realTimeWorker;
+    }
+}
