4685bf6e1153abfddd93ea3f9ec571e5ac5bc277
WooYeonJu <woojja07@naver.com>
2025-02-12 11:21:12 +0900

    Feat: 드롭다운 메뉴 추가, 캘린더 페이지 axios 연결

--- backend/src/main/java/com/albaro/controller/ChatController.java
+++ backend/src/main/java/com/albaro/controller/ChatController.java
@@ -14,23 +14,23 @@ import java.util.List;
 @Controller
 public class ChatController {
 
-    private final ChatService chatService;
-    private final SimpMessageSendingOperations messagingTemplate;
-
-    public ChatController(ChatService chatService, SimpMessageSendingOperations messagingTemplate) {
-        this.chatService = chatService;
-        this.messagingTemplate = messagingTemplate;
-    }
-
-    @MessageMapping("/chat.send")
-    public void sendMessage(@Payload ChatMessageDto message) {
-        chatService.sendMessage(message);
-        messagingTemplate.convertAndSend("/topic/store." + message.getStoreId(), message);
-    }
-
-    @GetMapping("/api/chat/{storeId}")
-    @ResponseBody
-    public List<ChatMessageDto> getChatHistory(@PathVariable Long storeId) {
-        return chatService.getChatMessages(storeId);
-    }
+//    private final ChatService chatService;
+//    private final SimpMessageSendingOperations messagingTemplate;
+//
+//    public ChatController(ChatService chatService, SimpMessageSendingOperations messagingTemplate) {
+//        this.chatService = chatService;
+//        this.messagingTemplate = messagingTemplate;
+//    }
+//
+//    @MessageMapping("/chat.send")
+//    public void sendMessage(@Payload ChatMessageDto message) {
+//        chatService.sendMessage(message);
+//        messagingTemplate.convertAndSend("/topic/store." + message.getStoreId(), message);
+//    }
+//
+//    @GetMapping("/api/chat/{storeId}")
+//    @ResponseBody
+//    public List<ChatMessageDto> getChatHistory(@PathVariable Long storeId) {
+//        return chatService.getChatMessages(storeId);
+//    }
 }
--- backend/src/main/java/com/albaro/controller/WorkInformationController.java
+++ backend/src/main/java/com/albaro/controller/WorkInformationController.java
@@ -3,15 +3,13 @@ package com.albaro.controller;
 import com.albaro.dto.WorkInformationResponse;
 import com.albaro.service.WorkInformationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/work-information")
+@RequestMapping("/api/work-information")
+@CrossOrigin(origins = "http://localhost:3000")
 public class WorkInformationController {
 
     @Autowired
@@ -20,6 +18,8 @@ public class WorkInformationController {
     // 특정 스토어의 모든 근무 정보 조회
     @GetMapping("/{storeId}")
     public List<WorkInformationResponse> getWorkInformationsByStoreId(@PathVariable Integer storeId) {
+        System.out.println(storeId);
+
         return workInformationService.getWorkInformationByStoreId(storeId);
     }
 }
