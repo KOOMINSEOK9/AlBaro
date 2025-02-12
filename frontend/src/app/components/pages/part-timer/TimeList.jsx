1019a49f578106fa1cccc142e8785d981b63efb9
WooYeonJu <woojja07@naver.com>
2025-02-12 13:45:16 +0900

    Fix: axios 경로 수정

--- frontend/src/app/components/pages/part-timer/TimeList.jsx
+++ frontend/src/app/components/pages/part-timer/TimeList.jsx
@@ -1,65 +1,65 @@
 import React, { useEffect } from "react";
 import TimeCard from "./TimeCard.jsx";
 
-// const times = [
-//   {
-//     workDate: "2025-02-01",
-//     startTime: "2025-02-01T08:00:00",
-//     endTime: "2025-02-01T12:00:00",
-//   },
-//   {
-//     workDate: "2025-02-01",
-//     startTime: "2025-02-01T14:00:00",
-//     endTime: "2025-02-01T18:00:00",
-//   },
-//   {
-//     workDate: "2025-02-02",
-//     startTime: "2025-02-02T09:00:00",
-//     endTime: "2025-02-02T13:00:00",
-//   },
-//   {
-//     workDate: "2025-02-02",
-//     startTime: "2025-02-02T15:00:00",
-//     endTime: "2025-02-02T19:00:00",
-//   },
-//   {
-//     workDate: "2025-02-03",
-//     startTime: "2025-02-03T07:00:00",
-//     endTime: "2025-02-03T11:00:00",
-//   },
-//   {
-//     workDate: "2025-02-03",
-//     startTime: "2025-02-03T13:00:00",
-//     endTime: "2025-02-03T17:00:00",
-//   },
-//   {
-//     workDate: "2025-02-04",
-//     startTime: "2025-02-04T08:30:00",
-//     endTime: "2025-02-04T12:30:00",
-//   },
-//   {
-//     workDate: "2025-02-04",
-//     startTime: "2025-02-04T16:00:00",
-//     endTime: "2025-02-04T20:00:00",
-//   },
-//   {
-//     workDate: "2025-02-05",
-//     startTime: "2025-02-05T09:30:00",
-//     endTime: "2025-02-05T13:30:00",
-//   },
-//   {
-//     workDate: "2025-02-05",
-//     startTime: "2025-02-05T14:30:00",
-//     endTime: "2025-02-05T18:30:00",
-//   },
-// ];
+const times = [
+  {
+    workDate: "2025-02-01",
+    startTime: "2025-02-01T08:00:00",
+    endTime: "2025-02-01T12:00:00",
+  },
+  {
+    workDate: "2025-02-01",
+    startTime: "2025-02-01T14:00:00",
+    endTime: "2025-02-01T18:00:00",
+  },
+  {
+    workDate: "2025-02-02",
+    startTime: "2025-02-02T09:00:00",
+    endTime: "2025-02-02T13:00:00",
+  },
+  {
+    workDate: "2025-02-02",
+    startTime: "2025-02-02T15:00:00",
+    endTime: "2025-02-02T19:00:00",
+  },
+  {
+    workDate: "2025-02-03",
+    startTime: "2025-02-03T07:00:00",
+    endTime: "2025-02-03T11:00:00",
+  },
+  {
+    workDate: "2025-02-03",
+    startTime: "2025-02-03T13:00:00",
+    endTime: "2025-02-03T17:00:00",
+  },
+  {
+    workDate: "2025-02-04",
+    startTime: "2025-02-04T08:30:00",
+    endTime: "2025-02-04T12:30:00",
+  },
+  {
+    workDate: "2025-02-04",
+    startTime: "2025-02-04T16:00:00",
+    endTime: "2025-02-04T20:00:00",
+  },
+  {
+    workDate: "2025-02-05",
+    startTime: "2025-02-05T09:30:00",
+    endTime: "2025-02-05T13:30:00",
+  },
+  {
+    workDate: "2025-02-05",
+    startTime: "2025-02-05T14:30:00",
+    endTime: "2025-02-05T18:30:00",
+  },
+];
 
 const TimeList = ({
   selectedStore,
   selectedDate,
   workStartTime,
   workEndTime,
-  times,
+  // times,
 }) => {
   const selectedDateString = selectedDate
     ? selectedDate.toISOString().split("T")[0]
