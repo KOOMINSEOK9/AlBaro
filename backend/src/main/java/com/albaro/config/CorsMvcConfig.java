5b338ee9469857f5f2a88d4c7e14d832fa514123
YOOSEUNGHO <tmdgh0124@korea.ac.kr>
2025-02-13 16:46:59 +0900

    localhost -> ip 주소로 변경 CORS 문제 해결 위해서

--- backend/src/main/java/com/albaro/config/CorsMvcConfig.java
+++ backend/src/main/java/com/albaro/config/CorsMvcConfig.java
@@ -11,7 +11,8 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry corsRegistry) {
 
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                // .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://i12b105.p.ssafy.io/:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
