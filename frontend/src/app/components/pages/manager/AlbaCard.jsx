8f585e70a1d6fe091d6706e30a6808960b873c9e
WooYeonJu <woojja07@naver.com>
2025-02-14 12:52:57 +0900

    test

--- frontend/src/app/components/pages/manager/AlbaCard.jsx
+++ frontend/src/app/components/pages/manager/AlbaCard.jsx
@@ -21,7 +21,6 @@ const AlbaCard = ({ albas }) => {
     ) {
       axios.post(
         `http://i12b105.p.ssafy.io:8080/api/substitute/request-to-worker`,
-
         {
           params: {
             userId,
