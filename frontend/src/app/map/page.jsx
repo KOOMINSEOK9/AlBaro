4e732b5d0943d1b2cbef33d7e12ef5ffb6546291
YOOSEUNGHO <tmdgh0124@korea.ac.kr>
2025-02-14 17:06:11 +0900

    Test

--- frontend/src/app/map/page.jsx
+++ frontend/src/app/map/page.jsx
@@ -7,29 +7,9 @@ import KakaoMap from "../components/pages/common/KakaoMap.jsx";
 import Header from "../components/common/header.jsx";
 import Footer from "../components/common/footer.jsx";
 
-// useIsMounted 훅 구현
-const useIsMounted = () => {
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  return isMounted;
-};
-
 const Map = () => {
   const router = useRouter();
   const [eventData, setEventData] = useState(null);
-
-  const searchParams = useSearchParams();
-  const getScheduleId = searchParams.get("scheduleId");
-  const getDate = searchParams.get("date");
-  const getStart = searchParams.get("start");
-  const getEnd = searchParams.get("end");
-
-  // 클라이언트에서만 Suspense를 적용하기 위해 useIsMounted 사용
-  const isMounted = useIsMounted();
 
   return (
     <div className="min-h-screen overflow-hidden flex flex-col bg-[#eee] text-black">
@@ -38,24 +18,32 @@ const Map = () => {
 
       {/* 본문 (스크롤 가능) */}
       <section className="flex-1 flex-grow overflow-hidden bg-[#fff]">
-        {isMounted ? (
-          <Suspense fallback={<div>Loading...</div>}>
-            <KakaoMap
-              scheduleId={getScheduleId}
-              date={getDate}
-              start={getStart}
-              end={getEnd}
-            />
-          </Suspense>
-        ) : (
-          <div>Loading...</div>
-        )}
+        <Suspense fallback={<div>Loading map...</div>}>
+          {/* useSearchParams를 Suspense 내부로 이동 */}
+          {() => {
+            const searchParams = useSearchParams();
+            const getScheduleId = searchParams.get("scheduleId");
+            const getDate = searchParams.get("date");
+            const getStart = searchParams.get("start");
+            const getEnd = searchParams.get("end");
+
+            return (
+              <KakaoMap
+                scheduleId={getScheduleId}
+                date={getDate}
+                start={getStart}
+                end={getEnd}
+              />
+            );
+          }}
+        </Suspense>
       </section>
 
-      {/* 푸터 */}
+      {/* 푸터 (필요 시 활성화) */}
       {/* <Footer /> */}
     </div>
   );
 };
 
 export default Map;
+
