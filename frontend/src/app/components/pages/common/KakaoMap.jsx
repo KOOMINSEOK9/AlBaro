41fe0552b4579fe3cc612c1e853495a4e62b36ae
WooYeonJu <woojja07@naver.com>
2025-02-14 12:36:24 +0900

    Fix: 지도 useSearchParams 오류(빌드 오류) 해결

--- frontend/src/app/components/pages/common/KakaoMap.jsx
+++ frontend/src/app/components/pages/common/KakaoMap.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Suspense } from "react";
+
 import TimeList from "../part-timer/TimeList.jsx";
 import AlbaList from "../manager/AlbaList.jsx";
 import DatePickerModule from "./DatePicker.jsx";
@@ -381,7 +383,13 @@ const KakaoMap = ({ scheduleId, date, start, end }) => {
 
         {/* 지도 영역 */}
         <div className="flex-grow bg-[#eee] p-3">
-          <div id="map" className="w-full h-full rounded-lg"></div>
+          <Suspense fallback={<div>Loading map...</div>}>
+            {storeData ? (
+              <div id="map" className="w-full h-full rounded-lg"></div>
+            ) : (
+              <div>Loading store data...</div>
+            )}
+          </Suspense>{" "}
         </div>
       </article>
     </div>
