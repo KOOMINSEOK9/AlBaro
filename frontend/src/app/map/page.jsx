274062cf2f91badeba5b39dff03df911069f5778
WooYeonJu <woojja07@naver.com>
2025-02-14 20:03:09 +0900

    Fix: 지도 빌드 오류 제발제발

--- frontend/src/app/map/page.jsx
+++ frontend/src/app/map/page.jsx
@@ -11,6 +11,16 @@ const Map = () => {
   const router = useRouter();
   const [eventData, setEventData] = useState(null);
 
+  const useIsMounted = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+      setIsMounted(true);
+    }, []);
+
+    return isMounted;
+  };
+
   // const searchParams = useSearchParams();
   // const getScheduleId = searchParams.get("scheduleId");
   // const getDate = searchParams.get("date");
@@ -18,7 +28,7 @@ const Map = () => {
   // const getEnd = searchParams.get("end");
 
   // 클라이언트에서만 Suspense를 적용하기 위해 useIsMounted 사용
-  // const isMounted = useIsMounted();
+  const isMounted = useIsMounted();
 
   return (
     <div className="min-h-screen overflow-hidden flex flex-col bg-[#eee] text-black">
