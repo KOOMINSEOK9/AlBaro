2969b91dcedb7d614e2bccf1fa487feb0ea68d6e
Yu Junghyun <a1017u@naver.com>
2025-02-15 13:51:27 +0900

    feat: 대타 요청 기능 수정

--- backend/src/main/java/com/albaro/controller/SubstitutionController.java
+++ backend/src/main/java/com/albaro/controller/SubstitutionController.java
@@ -202,8 +202,6 @@ public class SubstitutionController {
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.TIME) LocalTime startTime,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.TIME) LocalTime endTime) {
 
-
-
         substitutionService.requestSubstitution(userId, userName,
                 workDate, startTime, endTime);
         return ResponseEntity.ok().build();
