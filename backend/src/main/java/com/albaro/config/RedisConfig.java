1ae53a7144589ea8f7fdccf640e6ed0423f4b84b
YOOSEUNGHO <tmdgh0124@korea.ac.kr>
2025-02-19 05:03:42 +0900

    test

--- backend/src/main/java/com/albaro/config/RedisConfig.java
+++ backend/src/main/java/com/albaro/config/RedisConfig.java
@@ -5,12 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import com.albaro.dto.ChatMessageDto;
-import com.albaro.service.RedisSubscriber;
 
 // @Configuration
 // public class RedisConfig {
