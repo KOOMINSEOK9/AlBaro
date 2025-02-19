eba49e5d8db5ce801c05a31234088c3e66e5369e
WooYeonJu <woojja07@naver.com>
2025-02-19 15:08:24 +0900

    Feat: 대타구하기 로직 프로필 이미지 띄우기

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,15 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    async rewrites() {
-      return [
-        {
-          source: '/ws-stomp/:path*',
-          destination: 'https://i12b105.p.ssafy.io/ws-stomp/:path*' // 배포 환경
-        }
-      ]
-    }
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "albaro.s3.ap-northeast-2.amazonaws.com",
+      },
+    ],
+  },
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/ws-stomp/:path*",
+        destination: "https://i12b105.p.ssafy.io/ws-stomp/:path*", // 배포 환경
+      },
+    ];
+  },
+};
 
 // module.exports = nextConfig 대신
 export default nextConfig;
