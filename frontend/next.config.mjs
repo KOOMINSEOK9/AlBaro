c8b8d233d751e76bad5ffc8f820749b8d67a9fdc
YOOSEUNGHO <tmdgh0124@korea.ac.kr>
2025-02-19 10:01:47 +0900

    test

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    async rewrites() {
+      return [
+        {
+          source: '/ws-stomp/:path*',
+        //   destination: 'http://localhost:8080/ws-stomp/:path*' // 개발 환경
+          destination: 'https://i12b105.p.ssafy.io/ws-stomp/:path*' // 배포 환경
+        }
+      ]
+    }
+  }
+  
+  module.exports = nextConfig
