34c59041d67d951c55f95ad6306449d4a8f28747
YOOSEUNGHO <tmdgh0124@korea.ac.kr>
2025-02-19 11:48:41 +0900

    test

--- backend/src/main/java/com/albaro/config/WebSocketConfig.java
+++ backend/src/main/java/com/albaro/config/WebSocketConfig.java
@@ -12,25 +12,25 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-stomp")
+                .setAllowedOriginPatterns("*")  // 개발 중에는 모든 origin 허용
+                .withSockJS()
+                .setClientLibraryUrl("https://cdn.jsdelivr.net/npm/sockjs-client@1/dist/sockjs.min.js")
+                .setWebSocketEnabled(true)
+                .setSessionCookieNeeded(false);
+    }
+
+    @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/sub");
         config.setApplicationDestinationPrefixes("/pub");
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp")
-                .setAllowedOrigins("https://i12b105.p.ssafy.io")
-                .withSockJS()
-                .setStreamBytesLimit(512 * 1024)
-                .setHttpMessageCacheSize(1000)
-                .setDisconnectDelay(30 * 1000);
-    }
-
-    @Override
     public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
-        registration.setMessageSizeLimit(512 * 1024);
-        registration.setSendBufferSizeLimit(1024 * 1024);
-        registration.setSendTimeLimit(20 * 1000);
+        registration.setMessageSizeLimit(128 * 1024)
+                   .setSendBufferSizeLimit(512 * 1024)
+                   .setSendTimeLimit(20000);
     }
 }
