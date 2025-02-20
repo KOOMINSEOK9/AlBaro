698fb059efef333ce7321a6804f975e05f60a3b0
YOOSEUNGHO <tmdgh0124@korea.ac.kr>
2025-02-21 03:10:39 +0900

    test

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -9,15 +9,15 @@ const nextConfig = {
     ],
   },
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/ws/:path*",
-        // destination: "http://localhost:8080/ws/:path*", // 개발 환경
-        destination: "http://backend:8080/ws/:path*", // 배포 환경 - Docker 내부 네트워크 사용
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/ws/:path*",
+  //       // destination: "http://localhost:8080/ws/:path*", // 개발 환경
+  //       destination: "http://backend:8080/ws/:path*", // 배포 환경 - Docker 내부 네트워크 사용
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
