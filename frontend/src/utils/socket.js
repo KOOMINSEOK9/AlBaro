7350e9609e87e99b4880c71b058ee19f5238a379
YOOSEUNGHO <tmdgh0124@korea.ac.kr>
2025-02-20 15:24:49 +0900

    test

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -2,7 +2,8 @@ import SockJS from 'sockjs-client';
 import { Stomp } from '@stomp/stompjs';
 
 const SOCKET_URL = process.env.NODE_ENV === 'production'
-  ? 'https://i12b105.p.ssafy.io/ws-stomp'
+  // ? 'https://i12b105.p.ssafy.io/ws-stomp'
+  ? 'https://i12b105.p.ssafy.io'
   : 'http://localhost:8080/ws-stomp';
 
 let stompClient = null;
