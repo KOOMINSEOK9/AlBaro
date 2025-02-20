c6433cf8af9f739c888c917593f550b78608ac17
YOOSEUNGHO <tmdgh0124@korea.ac.kr>
2025-02-20 21:28:23 +0900

    test

--- backend/src/main/java/com/albaro/config/WebSocketConfig.java
+++ backend/src/main/java/com/albaro/config/WebSocketConfig.java
@@ -14,7 +14,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setAllowedOriginPatterns("*")
-                .withSockJS();
+                .withSockJS()
+                .setWebSocketEnabled(true)
+                .setHeartbeatTime(25000)
+                .setDisconnectDelay(5000)
+                .setClientLibraryUrl("https://cdnjs.cloudflare.com/ajax/libs/sockjs-client/1.5.1/sockjs.min.js");
     }
 
     @Override
