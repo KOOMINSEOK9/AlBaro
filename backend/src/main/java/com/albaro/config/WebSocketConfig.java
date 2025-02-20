696c35dfb551a0b05b07ee41aee147a44fdfcb5e
YOOSEUNGHO <tmdgh0124@korea.ac.kr>
2025-02-20 20:44:23 +0900

    test

--- backend/src/main/java/com/albaro/config/WebSocketConfig.java
+++ backend/src/main/java/com/albaro/config/WebSocketConfig.java
@@ -3,7 +3,6 @@ package com.albaro.config;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.converter.MessageConverter;
@@ -13,8 +12,6 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
-import org.springframework.scheduling.TaskScheduler;
 
 import java.util.List;
 
@@ -23,33 +20,24 @@ import java.util.List;
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Bean
-    public TaskScheduler messageBrokerTaskScheduler() {
-        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
-        scheduler.setPoolSize(1);
-        scheduler.setThreadNamePrefix("wss-heartbeat-thread-");
-        scheduler.setDaemon(true);
-        return scheduler;
-    }
-
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins(
-                    "https://i12b105.p.ssafy.io",
-                    "http://i12b105.p.ssafy.io"
-                )
+                .setAllowedOrigins("https://i12b105.p.ssafy.io")
                 .withSockJS()
                 .setWebSocketEnabled(true)       // WebSocket 활성화 명시적 설정
                 .setHeartbeatTime(25000)        // 하트비트 시간 설정
                 .setDisconnectDelay(5000);      // 연결 해제 지연 시간
+
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("https://i12b105.p.ssafy.io")     ;           
     }
+
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/sub")
-              .setHeartbeatValue(new long[]{10000, 10000})
-              .setTaskScheduler(messageBrokerTaskScheduler()); // TaskScheduler 설정 추가
+              .setHeartbeatValue(new long[]{10000, 10000}); // 브로커 하트비트 설정
         config.setApplicationDestinationPrefixes("/pub");
     }
 
