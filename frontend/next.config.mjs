fbd5e686db5c91792bf3174554659c2910c31459
YOOSEUNGHO <tmdgh0124@korea.ac.kr>
2025-02-20 22:26:58 +0900

    test

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -13,11 +13,11 @@ const nextConfig = {
     return [
       {
         source: "/ws/:path*",
-        destination: "https://i12b105.p.ssafy.io/ws/:path*", // 배포 환경
+        // destination: "http://localhost:8080/ws/:path*", // 개발 환경
+        destination: "http://backend:8080/ws/:path*", // 배포 환경 - Docker 내부 네트워크 사용
       },
     ];
   },
 };
 
-// module.exports = nextConfig 대신
 export default nextConfig;
