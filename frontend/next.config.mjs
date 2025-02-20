a94a2605bab2b67afe9fde1d3b61a53f2c5551b5
YOOSEUNGHO <tmdgh0124@korea.ac.kr>
2025-02-21 03:28:56 +0900

    test

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -9,15 +9,18 @@ const nextConfig = {
     ],
   },
   reactStrictMode: true,
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/ws/:path*",
-  //       // destination: "http://localhost:8080/ws/:path*", // 개발 환경
-  //       destination: "http://backend:8080/ws/:path*", // 배포 환경 - Docker 내부 네트워크 사용
-  //     },
-  //   ];
-  // },
+  // 웹소켓 요청을 Next.js가 처리하지 않도록 설정
+  async headers() {
+    return [
+      {
+        source: "/ws/:path*",
+        headers: [
+          { key: "Connection", value: "upgrade" },
+          { key: "Upgrade", value: "websocket" }
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -9,7 +9,7 @@ export const connectWebSocket = (onMessageReceived, storeId) => {
   }
 
   const socket = new SockJS('https://i12b105.p.ssafy.io/ws', null, {
-    transports: ['websocket', 'xhr-streaming', 'xhr-polling'],
+    transports: ['websocket'],
     timeout: 10000, // 10초
   });
   
